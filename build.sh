1adf74f50cbc7b60575cbe14246d33ec2b490604
Joshua Hayworth <Joshua.Hayworth@kratosdefense.com>
2022-07-21 16:27:22 -0700

    Added a few pauses in apply execution script

--- build.sh
+++ build.sh
@@ -28,6 +28,13 @@ if [ "$1" == "apply" ]; then
     #generate_downloadartifacts_script
     generate_ansible_inventory
     terraform apply | tee terraform_run.log
+    read -p "Terraform should be done. Continue? ..."
+    
+    cd playbook
+    ansible-playbook -i inventory ping-k8s.yaml
+    read -p "Ping test done. Continue? ..."
+    
+    ansible-playbook -i inventory provision-k8s.yaml
 elif [ "$1" == "destroy" ]; then
     terraform destroy
 elif [ "$1" == "clean" ]; then
