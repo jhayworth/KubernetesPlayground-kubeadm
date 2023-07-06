c016f1d14494159383174faa418506f64cc53e3e
Joshua Hayworth <Joshua.Hayworth@kratosdefense.com>
2023-07-06 09:23:58 -0700

    Remove an old file that is no longer needed

--- client.rb
+++
@@ -1,5 +0,0 @@
-log_level        :info
-log_location     STDOUT
-chef_server_url  'https://chef.devops.kratosdefense.com/organizations/devops'
-validation_key         "/etc/chef/chef-validator.pem"
-validation_client_name 'devops-validator'
